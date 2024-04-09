6d0a576d3120ee6e3aee2fc4f3e48a50dd19ee87
Genesis McGruder <genesismcgruder@gmail.com>
2024-04-09 12:41:12 -0400

    done

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+  const [pokemons, setPokemons] = useState([])
+  const [search, setSearch] = useState("")
+  useEffect(()=>{
+    fetch('http://localhost:3001/pokemon')
+    .then(res => res.json())
+    .then(data => setPokemons(data))
+  }, [])
+
+  
+
+  function handleChange(e){
+    setSearch(e.target.value)
+  }
+
+    const filteredPokemon = pokemons.filter((pokemon)=> pokemon.name.toLowerCase().includes(search))
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
       <PokemonForm />
       <br />
-      <Search />
+      <Search  search={search} handleChange={handleChange}/>
       <br />
-      <PokemonCollection />
+      <PokemonCollection pokemons={pokemons} filteredPokemon={filteredPokemon} />
     </Container>
   );
 }
